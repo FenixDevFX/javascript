b38de8c91cd62b22035bd3744540fc393fcf381b
FenixDevFX <fenixlycanus@gmail.com>
2023-12-28 11:20:58 -0300

    feat: adiciona conteúdo dos exercícios 1 e 2 no arquivo aula_funcao2.js

---
+++ funcoes/aula_funcao2.js
@@ -0,0 +1,17 @@
+function escreverMeuNome (nome){
+    console.log('Meu nome é ' + nome);
+}
+escreverMeuNome ('Vitor');
+escreverMeuNome ('Renan');
+
+function verificarIdade (idade) {
+    if (idade >= 18) {
+        console.log('Maior de Idade');
+    } else {
+        console.log('Menor de Idade');
+    }
+}
+verificarIdade (15);
+
+
+
